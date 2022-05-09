a286611d06987f5362e5af8169ea979312d7b6ed
yuchen203 <79148954+yuchen203@users.noreply.github.com>
2022-05-09 17:57:46 +0800

    finish training and testing.

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import argparse
 import torch
 import torchvision.transforms as transforms
 from fer import FER2013
+import os
+import torch.optim as optim
+import torch.nn as nn
+import utils
+from torch.autograd import Variable
+import numpy as np
 
 # parser
 parser = argparse.ArgumentParser(description='attack to FER on CK+')
 parser.add_argument('--model', type=str, default='VGG19', choices=['VGG19', 'Resnet18'], help='CNN architecture')
-parser.add_argument('--fold', type=int, default=1, help='k fold number')
-parser.add_argument('--bs', type=int, default=128, help='batch size')
+parser.add_argument('--bs', type=int, default=32, help='batch size')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 opt = parser.parse_args()
 
-# Data
+# data
 cut_size = 44
 print('==> Preparing data..')
 transform_train = transforms.Compose([
@@ -33,5 +38,175 @@ PublicTestloader = torch.utils.data.DataLoader(PublicTestset, batch_size=opt.bs,
 PrivateTestset = FER2013(split='PrivateTest', transform=transform_test)
 PrivateTestloader = torch.utils.data.DataLoader(PrivateTestset, batch_size=opt.bs, shuffle=False, num_workers=0)
 
+# model
+if opt.model == 'VGG19':
+    net = torch.hub.load('pytorch/vision:v0.10.0', 'vgg19', pretrained=False)
+else:
+    net = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
+
+# train
+use_cuda = torch.cuda.is_available()
+best_PublicTest_acc = 0  # best PublicTest accuracy
+best_PublicTest_acc_epoch = 0
+best_PrivateTest_acc = 0  # best PrivateTest accuracy
+best_PrivateTest_acc_epoch = 0
+learning_rate_decay_start = 80  # 50
+learning_rate_decay_every = 5  # 5
+learning_rate_decay_rate = 0.9  # 0.9
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+total_epoch = 250
+path = os.path.join(opt.model)
+
+if opt.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir(path), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load(os.path.join(path, 'PrivateTest_model.t7'))
+    net.load_state_dict(checkpoint['net'])
+    best_PublicTest_acc = checkpoint['best_PublicTest_acc']
+    best_PrivateTest_acc = checkpoint['best_PrivateTest_acc']
+    best_PrivateTest_acc_epoch = checkpoint['best_PublicTest_acc_epoch']
+    best_PrivateTest_acc_epoch = checkpoint['best_PrivateTest_acc_epoch']
+    start_epoch = checkpoint['best_PrivateTest_acc_epoch'] + 1
+else:
+    print('==> Building model..')
+
+if use_cuda:
+    net.cuda()
+    print('==> Running on GPU..')
+else:
+    print('==> Running on CPU..')
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=opt.lr, momentum=0.9, weight_decay=5e-4)
+
+
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    global Train_acc
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    if epoch > learning_rate_decay_start >= 0:
+        frac = (epoch - learning_rate_decay_start) // learning_rate_decay_every
+        decay_factor = learning_rate_decay_rate ** frac
+        current_lr = opt.lr * decay_factor
+    else:
+        current_lr = opt.lr
+    utils.set_lr(optimizer, current_lr)  # set the decayed rate
+    print('learning_rate: %s' % str(current_lr))
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        utils.clip_gradient(optimizer, 0.1)
+        optimizer.step()
+        _, predicted = torch.max(outputs.data, 1)
+        train_loss += loss.data.item()
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+        utils.progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                           % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+    Train_acc = 100. * correct / total
+
+
+def PublicTest(epoch):
+    global PublicTest_acc
+    global best_PublicTest_acc
+    global best_PublicTest_acc_epoch
+    net.eval()
+    PublicTest_loss = 0
+    correct=0
+    total=0
+    for batch_idx, (inputs, targets) in enumerate(PublicTestloader):
+        bs, ncrops, c, h, w = np.shape(inputs)
+        inputs = inputs.view(-1, c, h, w)
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)
+        outputs_avg = outputs.view(bs, ncrops, -1).mean(1)  # avg over crops
+        loss = criterion(outputs_avg, targets)
+        PublicTest_loss += loss.data.item()
+        _, predicted = torch.max(outputs_avg.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        utils.progress_bar(batch_idx, len(PublicTestloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                           % (PublicTest_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+    # Save checkpoint.
+    PublicTest_acc = 100. * correct / total
+    if PublicTest_acc > best_PublicTest_acc:
+        print('Saving..')
+        print("best_PublicTest_acc: %0.3f" % PublicTest_acc)
+        state = {
+            'net': net.state_dict() if use_cuda else net,
+            'acc': PublicTest_acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        torch.save(state, os.path.join(path, 'PublicTest_model.t7'))
+        best_PublicTest_acc = PublicTest_acc
+        best_PublicTest_acc_epoch = epoch
+
+
+def PrivateTest(epoch):
+    global PrivateTest_acc
+    global best_PrivateTest_acc
+    global best_PrivateTest_acc_epoch
+    net.eval()
+    PrivateTest_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(PrivateTestloader):
+        bs, ncrops, c, h, w = np.shape(inputs)
+        inputs = inputs.view(-1, c, h, w)
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)
+        outputs_avg = outputs.view(bs, ncrops, -1).mean(1)  # avg over crops
+        loss = criterion(outputs_avg, targets)
+        PrivateTest_loss += loss.data.item()
+        _, predicted = torch.max(outputs_avg.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        utils.progress_bar(batch_idx, len(PublicTestloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (PrivateTest_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+    # Save checkpoint.
+    PrivateTest_acc = 100.*correct/total
+
+    if PrivateTest_acc > best_PrivateTest_acc:
+        print('Saving..')
+        print("best_PrivateTest_acc: %0.3f" % PrivateTest_acc)
+        state = {
+            'net': net.state_dict() if use_cuda else net,
+	        'best_PublicTest_acc': best_PublicTest_acc,
+            'best_PrivateTest_acc': PrivateTest_acc,
+    	    'best_PublicTest_acc_epoch': best_PublicTest_acc_epoch,
+            'best_PrivateTest_acc_epoch': epoch,
+        }
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        torch.save(state, os.path.join(path,'PrivateTest_model.t7'))
+        best_PrivateTest_acc = PrivateTest_acc
+        best_PrivateTest_acc_epoch = epoch
+
+
 if __name__ == '__main__':
-    print(opt.model)
+    for epoch in range(start_epoch, total_epoch):
+        train(epoch)
+        PublicTest(epoch)
+        PrivateTest(epoch)
+    print("best_PublicTest_acc: %0.3f" % best_PublicTest_acc)
+    print("best_PublicTest_acc_epoch: %d" % best_PublicTest_acc_epoch)
+    print("best_PrivateTest_acc: %0.3f" % best_PrivateTest_acc)
+    print("best_PrivateTest_acc_epoch: %d" % best_PrivateTest_acc_epoch)
