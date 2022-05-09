db606eeb0cab1eb61b51c78e3a941a3ad630d823
yuchen203 <79148954+yuchen203@users.noreply.github.com>
2022-05-09 12:59:16 +0800

    finish data loader.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import torch
+import torchvision.transforms as transforms
 
 # parser
 parser = argparse.ArgumentParser(description='attack to FER on CK+')
@@ -8,6 +10,18 @@ parser.add_argument('--bs', type=int, default=128, help='batch size')
 parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 opt = parser.parse_args()
+
+# data loader
+cut_size = 44
+transform_train = transforms.Compose([
+    transforms.RandomCrop(cut_size),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+transform_test = transforms.Compose([
+    transforms.TenCrop(cut_size),
+    transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+])
 
 if __name__ == '__main__':
     print(opt.model)
